f3f0d59fd8661ac1f11e2cc646e734dbdefcce49
Guillaume <guillaume@pelikan-mobility.com>
2026-08-25 22:14:34 +0200

    Use archive.org for most PDFs

--- books.js
+++ books.js
@@ -2,58 +2,64 @@ var data = data || {};
 data.books = [
   {
     "volume": 1,
-    "url": "https://www.swiss-jazz.ch/partitions-gratuites/Real-Book.pdf",
+    "url": "https://dn710608.ca.archive.org/0/items/Volume1C/Volume-1-C.pdf",
     "offsets": [{"offset": 13}],
     "name": "The Real Book - Volume 1 C"
   },
   {
     "volume": 1,
-    "url": "https://www.swiss-jazz.ch/Real-Book/Volume-1-contrebasse.pdf",
-    "offsets": [
-      {"to": 108, "offset": 10},
-      {"from": 109, "offset": 12}
-    ],
+    "url": "https://dn760100.eu.archive.org/0/items/fakebook_the-real-book---fifth-edition---f---bass-clef/therealbook-fifthedition-f-bassclef.pdf",
+    "offsets": [{"offset": 10}],
     "name": "The Real Book - Volume 1 Bass"
   },
   {
     "volume": 1,
-    "url": "https://www.swiss-jazz.ch/Real-Book/Volume-1-Bb.pdf",
-    "offsets": [{"offset": 10}],
+    "url": "https://dn760106.eu.archive.org/0/items/TheRealBook1InBb/realbook1bb.pdf",
+    "offsets": [
+      {"to": 37, "offset": 7},
+      {"from": 39, "offset": 6}
+    ],
     "name": "The Real Book - Volume 1 Bb"
   },
   {
     "volume": 1,
-    "url": "https://www.swiss-jazz.ch/Real-Book/Volume-1-Eb.pdf",
-    "offsets": [{"offset": 8}],
+    "url": "https://dn760005.eu.archive.org/0/items/RealBook_224/Volume1Eb.pdf",
+    "offsets": [{"offset": 10}],
     "name": "The Real Book - Volume 1 Eb"
   },
   {
     "volume": "1 Additions",
-    "url": "https://www.swiss-jazz.ch/partitions-gratuites/Real-Book.pdf",
+    "url": "https://dn710608.ca.archive.org/0/items/Volume1C/Volume-1-C.pdf",
     "offsets": [{"offset": 497}],
     "name": "The Real Book - Volume 1 C - Additions"
   },
   {
     "volume": "1 Additions",
-    "url": "https://www.swiss-jazz.ch/Real-Book/Volume-1-contrebasse.pdf",
-    "offsets": [{"offset": 496}],
+    "url": "https://dn760100.eu.archive.org/0/items/fakebook_the-real-book---fifth-edition---f---bass-clef/therealbook-fifthedition-f-bassclef.pdf",
+    "offsets": [{"offset": 494}],
     "name": "The Real Book - Volume 1 Bass - Additions"
   },
   {
     "volume": "1 Additions",
-    "url": "https://www.swiss-jazz.ch/Real-Book/Volume-1-Eb.pdf",
-    "offsets": [{"offset": 492}],
+    "url": "https://dn760106.eu.archive.org/0/items/TheRealBook1InBb/realbook1bb.pdf",
+    "offsets": [{"offset": 490}],
+    "name": "The Real Book - Volume 1 Bb - Additions"
+  },
+  {
+    "volume": "1 Additions",
+    "url": "https://dn760005.eu.archive.org/0/items/RealBook_224/Volume1Eb.pdf",
+    "offsets": [{"offset": 494}],
     "name": "The Real Book - Volume 1 Eb - Additions"
   },
   {
     "volume": 2,
-    "url": "https://www.swiss-jazz.ch/Real-Book/Volume-2-C.pdf",
+    "url": "https://dn760101.eu.archive.org/0/items/fakebook_the-real-book-volume-2/therealbookvolume2.pdf",
     "offsets": [{"offset": 7}],
     "name": "The Real Book - Volume 2 C"
   },
   {
     "volume": 2,
-    "url": "https://www.swiss-jazz.ch/Real-Book/Volume-2-contrebasse.pdf",
+    "url": "https://dn790000.ca.archive.org/0/items/fakebook_real-book-bass-vol-2/realbookbassvol2.pdf",
     "offsets": [
       {"to": 157, "offset": 8},
       {"from": 160, "to": 209, "offset": 6},
@@ -77,13 +83,13 @@ data.books = [
   },
   {
     "volume": 2,
-    "url": "https://www.swiss-jazz.ch/Real-Book/Volume-2-Eb.pdf",
+    "url": "https://dn760106.eu.archive.org/0/items/fakebook_real-book2-eb/realbook2eb.pdf",
     "offsets": [{"offset": 6}],
     "name": "The Real Book - Volume 2 Eb"
   },
   {
     "volume": 3,
-    "url": "https://www.swiss-jazz.ch/Real-Book/Volume-3-C.pdf",
+    "url": "https://dn760106.eu.archive.org/0/items/fakebook_the-real-book-volume-3/therealbookvolume3.pdf",
     "offsets": [{"offset": 5}],
     "name": "The Real Book - Volume 3 C"
   },
